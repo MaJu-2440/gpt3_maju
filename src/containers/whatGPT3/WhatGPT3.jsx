323b97011e4064650b295042310020acd35b13a1
MaJu <mariajuliapacheco2012@gmail.com>
2025-01-15 16:41:16 -0300

    first commit

---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './whatGPT3.css'
+import { Feature } from '../../components'
+
+const WhatGPT3 = () => {
+  return (
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt#__whatgpt3-features'>
+        <Feature title='What is GPT-3 ?' text= 'We so opinion friendsme message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by.'/>
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>
+          The possibilities beyond your imagination
+        </h1>
+        <p>Explore the library</p>
+      </div>
+
+      <div className='gpt3__whatgpt3-container'>
+        <Feature title='Chatbots' text='Extremity as if breakfast agreement. Off now mistress provided out horrible opinions. Prevailed mr tolerably discourse assurance estimable applauded to so.'/>
+        <Feature title='Knowledgebase' text='Him everything melancholy uncommonly but solicitude inhabiting projection off. Connection stimulated estimating excellence an to impression.'/>
+        <Feature title='Education' text='We so opinion friendsme message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own.'/>
+      </div>
+    </div>
+  )
+}
+
+export default WhatGPT3
